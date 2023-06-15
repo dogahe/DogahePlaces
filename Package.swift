518a84cb915291c62103a941d1bbb61322ec7044
dogahe <dogahe@google.com>
2023-06-15 14:41:15 -0400

    save

--- Package.swift
+++ Package.swift
@@ -4,27 +4,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "GooglePlaces",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "GooglePlaces",
-            targets: ["GooglePlacesTarget"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "GooglePlacesTarget",
-            dependencies: [.target(name: "GooglePlaces")],
-            path: "GooglePlacesTarget"
-        ),
-        .binaryTarget(name: "GooglePlaces", url: "https://github.com/dogahe/DogahePlaces/releases/download/1.0.0/GooglePlaces_3p.xcframework.zip", checksum: "c16e1df42f21228783a13a06e008888322f4f317d1ae0dec54f0d029172138f2"),
-    ]
+  name: "GooglePlaces",
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "GooglePlaces",
+      targets: ["GooglePlacesTarget"]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "GooglePlacesTarget",
+      dependencies: [.target(name: "GooglePlaces")],
+      path: "GooglePlacesTarget",
+      resources: [
+        .copy("GooglePlaces.bundle")
+      ]
+    ),
+    .binaryTarget(name: "GooglePlaces", url: "https://github.com/dogahe/DogahePlaces/releases/download/1.0.0/GooglePlaces_3p.xcframework.zip", checksum: "c16e1df42f21228783a13a06e008888322f4f317d1ae0dec54f0d029172138f2"),
+  ]
 )
 
 /*
