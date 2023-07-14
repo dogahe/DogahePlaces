e0d7ffbb2b0aef2a21846e99e59d72786839250d
Behzad Dogahe <dogahe@google.com>
2023-07-14 15:36:49 -0400

    rename the target name

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
       name: "GooglePlaces",
-      targets: ["GooglePlacesTarget"]),
+      targets: ["GooglePlaces"]),
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
-      name: "GooglePlacesTarget",
+      name: "GooglePlaces",
       dependencies: [.target(name: "GooglePlaces")],
       path: "GooglePlacesTarget",
       resources: [
