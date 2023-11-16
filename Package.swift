14357f5f08cded9e4c721124bd35f0f47b704ce8
dogahe <dogahe@google.com>
2023-11-16 11:47:31 -0500

    correct lorry link

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
-    .binaryTarget(name: "GooglePlaces", url: "https://dl.google.com/geosdk/swiftpm/8.3.0/GooglePlacesResources.zip", checksum: "fc5539677cdf6c0ce32f04af6f347abdc918f49c5b84a60d6ea90eef058b2ef3"),
+    .binaryTarget(name: "GooglePlaces", url: "https://dl.google.com/geosdk/swiftpm/8.3.0/GooglePlaces_3p.xcframework.zip", checksum: "fc5539677cdf6c0ce32f04af6f347abdc918f49c5b84a60d6ea90eef058b2ef3"),
     .target(
       name: "GooglePlacesTarget",
       dependencies: ["GooglePlaces"],
